8da84f1161e6693543fa6aa2e5e85a69eefc7d70
ETK <eaintthwinkyaw.asiamatrix@gmail.com>
2024-08-11 22:16:54 +0630

    disable_quick_create

---
+++ disable_quick_create/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Disable Easy Create',
+
+    'version': '17.1.1',
+
+    'summary': 'Disable Easy Create',
+
+    'category': 'Technichal/Settings',
+
+    'website': 'https://www.asiamatrixsoftware.com',
+
+    'depends': [
+        'web',
+    ],
+
+    'description': """
+        V1.0 - Disable Create and Edit, Disable Quick Create, Disable Create
+               (Remark: You can management with manual open this options)
+        V1.1 - Add Many2manyfields
+    """,
+
+    "maintainers": ["dhongu"],
+
+    "assets": {
+        "web.assets_backend": [
+            "disable_quick_create/static/src/js/fields.esm.js",
+        ]
+    },
+
+    'installable': True,
+
+    'application': True,
+
+    'auto_install': False,
+
+    'license': 'LGPL-3',
+
+    'images': ['static/description/icon.png'],
+}
