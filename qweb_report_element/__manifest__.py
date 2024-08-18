831df38e468c119016444474b482365ba1117976
Nexus <nexus.ways888@example.com>
2024-08-18 23:18:54 +0630

    qweb_report_element

---
+++ qweb_report_element/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Qweb Report Elements',
+
+    'version': '1.0',
+
+    'summary': 'Qweb Report Elements',
+
+    'description': """
+        New elements for pdf class
+        ** first-page
+        ** last-page
+    """,
+
+    'category': 'Reports',
+
+    'Author': 'Nexus',
+
+    'license': "LGPL-3",
+
+    'email': 'nexus.innovatech.yours@gmail.com',
+
+    'depends': [
+        'web',
+    ],
+
+    'data': [
+        'views/layouts.xml',
+    ],
+
+    'installable': True,
+
+    'application': False,
+
+    'auto_install': False,
+
+    'images': ['static/description/icon.png'],
+}
